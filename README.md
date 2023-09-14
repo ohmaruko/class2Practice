e7941c2fad87f019764024a34b0f114502b20327
madoka <mnogaki@my.bcit.ca>
2023-09-13 23:57:35 -0700

    update class2practice file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Basic Operation System and Prog
+## class2
+### **Declaring**, initializing, assigning
+
+This is a quote
+> this is a quote
+- apple
+* orange
++ grape
+2 apple
+2 orange
+5 grape
---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Declare
+ */
+var computer;
+var pen;
+var paperBag;
+var outdoorActivities;
+
+//declare then assign
+var cat; //declare
+cat = 1; //assign
+console.log(cat);
+var lightBulbs;
+lightBulbs = 4;
+console.log(lightBulbs);
+var brand;
+brand = "Google";
+console.log(brand);
+var jobTitle;
+jobTitle = "Website Developer"
+console.log(jobTitle);
+
+
+//initialize
+var chips = 100;
+console.log(chips);
+var name = "Fred";
+console.log(name);
+var cleaningDuster = 1;
+console.log(cleaningDuster);
